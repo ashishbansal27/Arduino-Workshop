5bcf0a525b3ff52913df2cb9afd9b079c9ce1c6f
Ashish Bansal <ashish.bansal.foru@gmail.com>
2019-09-29 14:24:48 +0530

    Update Thermometer.ino

--- DigitalThermometer/Thermometer/Thermometer.ino
+++ DigitalThermometer/Thermometer/Thermometer.ino
@@ -8,7 +8,7 @@ void loop() {
   int temp_adc_val;
   float temp_val;
   temp_adc_val = analogRead(lm35_pin);  /* Read Temperature */
-  temp_val = (temp_adc_val * 4.88); /* Convert adc value to equivalent voltage. ADC 1023 = 5v ; ADC 1 = 5/1023 V ; 1 = 0.00488 V ; 4.88 mV ; 
+  temp_val = (temp_adc_val * 4.88); /* Convert adc value to equivalent voltage. ADC 1023 = 5v ; ADC 1 = 5/1023 V ; 1 = 0.00488 V ; 4.88 mV ; */
   temp_val = (temp_val/10); /* LM35 gives output of 10mv/°C */
   Serial.print("Temperature = ");
   Serial.print(temp_val);
